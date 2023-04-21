0895db7f917f5ec917429f2de25342e3abd836ce
Devrath Rana <devrath.rana@kellton.com>
2023-04-22 03:30:02 +0530

    projrct

---
+++ services/index.ts
@@ -0,0 +1,39 @@
+import { request, gql } from 'graphql-request';
+
+const graphqlApi: any = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+export const getPosts = async () => {
+    const query = gql`
+        query MyQuery {
+            postsConnection {
+                edges {
+                    node {
+                        author {
+                            bio
+                            name
+                            id
+                            photo {
+                                url
+                            }
+                        }
+                        createdAt
+                        slug
+                        title
+                        excerpt
+                        featuredImage {
+                            url
+                        }
+                        categories {
+                            name
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `;
+
+    const result: any = await request(graphqlApi, query);
+
+    return result.postsConnection.edges;
+};
